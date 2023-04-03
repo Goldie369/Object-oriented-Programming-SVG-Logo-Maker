368e2f6ab97ab7a37b1d170cf28be635d76fc135
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-03 17:51:52 -0500

    adding a Inquirer promp and connecting the foldes

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require ('inquirer'); 
+
+const fs = require ('fs'); 
+
+const Circle = require ('./lib/circle'); 
+
+const Triangle = require ('./lib/triangle');
+
+const Square = require ('./lib/square');
