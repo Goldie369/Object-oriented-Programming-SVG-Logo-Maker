5b575c13209bc47f20b81179bb7ee6f5d10a3322
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-04 20:21:31 -0500

    adding a array of questions

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ const Triangle = require ('./lib/triangle');
 
 const Square = require ('./lib/square');
 
+const questions = [
+   
+    {
+        type: 'input', 
+        message: 'Enter Text for the Logo. (Must not be more than 3 characters).',
+        name: 'logo',
+        validate: logoText, 
+
+    }, 
+    {
+        type: 'input', 
+        message: 'Enter a Text color.', 
+        name: 'textcolor',
+        validate: validHexaCode,
+
+    }, 
+    {
+        type: 'list', 
+        message: 'Select a shape for the Logo.', 
+        name: 'shape', 
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input', 
+        message: 'Enter a Shape color.', 
+        name: 'shapecolor',  
+        validate: validHexaCode,
+
+    },
+];
